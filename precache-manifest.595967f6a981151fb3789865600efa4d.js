e2e2c720c916e9d7ff425797cb929551ca7c8d71
Ar-404 <wolfweb19@gmail.com>
2020-08-06 00:29:30 +0530

    Updates

---
+++ precache-manifest.595967f6a981151fb3789865600efa4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf6380e402ed882b17c11f7c985a732",
+    "url": "/github.com/React1Project/index.html"
+  },
+  {
+    "revision": "670dbb205665260ecc2b",
+    "url": "/github.com/React1Project/static/css/main.2691e2f6.chunk.css"
+  },
+  {
+    "revision": "5e15aaf6042ba71f1271",
+    "url": "/github.com/React1Project/static/js/2.e658f16d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.com/React1Project/static/js/2.e658f16d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "670dbb205665260ecc2b",
+    "url": "/github.com/React1Project/static/js/main.77d7253e.chunk.js"
+  },
+  {
+    "revision": "7abfe5baac43e94a8898",
+    "url": "/github.com/React1Project/static/js/runtime-main.117f67fd.js"
+  }
+]);
